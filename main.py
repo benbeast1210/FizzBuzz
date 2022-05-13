4da03b81c2a4dd9aae6f1943b49d865282cc4bf6
Benjamin Sams <administrator@administrators-MacBook-Pro.local>
2022-05-13 14:26:45 -0400

    initial-commit

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+def fizzbuzz(n):
+
+  for x in range(1, n, 2):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
